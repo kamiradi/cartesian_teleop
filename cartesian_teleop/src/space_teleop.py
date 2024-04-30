1252b0b19c9e6233cdd9758a1db5b75778777413
kamiradi <adityakamireddypalli@gmail.com>
2024-04-30 19:00:28 +0100

    [WIP] teleop working but angle twist incorrect

--- cartesian_teleop/src/space_teleop.py
+++ cartesian_teleop/src/space_teleop.py
@@ -12,7 +12,7 @@ from absl import app, flags, logging
 from scipy.spatial.transform import Rotation as R
 import os
 from pydrake.all import (
-    RigidTransform
+    RigidTransform, Quaternion, RollPitchYaw
 )
 
 FLAGS = flags.FLAGS
@@ -20,6 +20,28 @@ flags.DEFINE_string(
     "robot_ip", None, "IP address of the robot.", required=True)
 flags.DEFINE_string("load_gripper", 'false',
                     "Whether or not to load the gripper.")
+
+
+def _write_pose_msg(X_AB, p, q):
+    # p - position message
+    # q - quaternion message
+    X_AB = RigidTransform(X_AB)
+    p.x, p.y, p.z = X_AB.translation()
+    q.w, q.x, q.y, q.z = X_AB.rotation().ToQuaternion().wxyz()
+
+
+def to_ros_transform(X_AB):
+    """Converts Drake transform to ROS transform."""
+    msg = geom_msg.Transform()
+    _write_pose_msg(X_AB, p=msg.translation, q=msg.rotation)
+    return msg
+
+
+def to_ros_pose(X_AB):
+    """Converts Drake transform to ROS pose."""
+    msg = geom_msg.Pose()
+    _write_pose_msg(X_AB, p=msg.position, q=msg.orientation)
+    return msg
 
 
 def joyCallback(data):
@@ -129,12 +151,15 @@ class SpacenavTeleOperator(object):
     """
     This class is responsible for relaying spacenav twists as equilibrium poses
     to the cartesian controller
+
     """
 
     def __init__(self, name):
         # initialisation message
         self._name = name
         rospy.loginfo("%s: Spacenav teleop node initialized", self._name)
+        self._debg_cnt = 0
+        self._scale = 0.05
 
         # Declare subscribers
         self._joy_sub = rospy.Subscriber(
@@ -144,11 +169,12 @@ class SpacenavTeleOperator(object):
         )
         self._tfBuffer = tf2_ros.Buffer()
         self._tfListener = tf2_ros.TransformListener(self._tfBuffer)
-        self._equilibrium_pose_sub = rospy.Subscriber(
-            "/cartesian_impedance_controller/equilibrium_pose",
-            geom_msg.PoseStamped,
-            self._poseCallback
-        )
+        self._tfBr = tf2_ros.TransformBroadcaster()
+        # self._equilibrium_pose_sub = rospy.Subscriber(
+        #     "/cartesian_impedance_controller/equilibrium_pose",
+        #     geom_msg.PoseStamped,
+        #     self._poseCallback
+        # )
         self._equilibrium_pose_pub = rospy.Publisher(
             "/cartesian_impedance_controller/equilibrium_pose",
             geom_msg.PoseStamped,
@@ -156,23 +182,49 @@ class SpacenavTeleOperator(object):
         )
 
     def _joyCallback(self, msg):
-        # rospy.loginfo("%s: Tele-operation node running: left button state, %s"
-        #               %(self._name, msg.buttons[0]))
+        # TODO: remove hard coded tf frame ids
         try:
-            X_EE = self._tfBuffer.lookup_transform(
+            tf_X_EE = self._tfBuffer.lookup_transform(
                 'panda_link0',
                 'panda_EE',
                 rospy.Time())
+            translation = tf_X_EE.transform.translation
+            ori = tf_X_EE.transform.rotation
+
+            dr_quat = Quaternion(np.array([ori.w, ori.x, ori.y, ori.z]))
+            X_EE = RigidTransform(dr_quat, np.array(
+                [translation.x,
+                 translation.y,
+                 translation.z]))
         except (tf2_ros.LookupException):
             return
-        rospy.loginfo("Tele-operation node: end effector transform: %s"
-                      % (X_EE.transform))
-        rospy.loginfo("Tele-operation node: axes state, %s"
-                      % (msg.axes[0]))
+        # rospy.loginfo("Tele-operation node: axes state, %s"
+        #               % (msg.axes[0]))
 
         # Just update xy messages
-        x = msg.axes[0]
-        y = msg.axes[1]
+        x = msg.axes[0] * self._scale
+        y = msg.axes[1] * self._scale
+        z = -msg.axes[2] * self._scale
+        roll = msg.axes[3] * self._scale
+        pitch = msg.axes[4] * self._scale
+        yaw = msg.axes[5] * self._scale
+        X_EE_delta = RigidTransform(
+            rpy=RollPitchYaw([roll, pitch, yaw]),
+            p=[x, y, z])
+        X_EEnew = X_EE @ X_EE_delta
+        t = geom_msg.TransformStamped()
+        t.header.stamp = rospy.Time.now()
+        t.header.frame_id = "panda_link0"
+        t.child_frame_id = "wp-{}".format(self._debg_cnt)
+        t.transform = to_ros_transform(X_EEnew)
+        self._tfBr.sendTransform(t)
+
+        pose = geom_msg.PoseStamped()
+        pose.header.stamp = rospy.Time.now()
+        pose.header.frame_id = "e_pose"
+        pose.pose = to_ros_pose(X_EEnew)
+
+        self._equilibrium_pose_pub.publish(pose)
 
     def _poseCallback(self, msg):
         rospy.loginfo("Current equilibrium pose of the robot: \n %s"
